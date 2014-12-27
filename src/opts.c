c5053717bcbd2f145eda672fde62d3fb17ace2b7
Jacob Adams <tookmund@gmail.com>
2014-12-27 11:03:14 -0500

    Use ETCPREFIX instead of PREFIX

--- src/opts.c
+++ src/opts.c
@@ -49,7 +49,7 @@ static const char copyright[] = "\n"
 
 
 #ifndef NO_CONFIG
-static char configfile[PATH_MAX] = PREFIX"/etc/swapspace.conf";
+static char configfile[PATH_MAX] = ETCPREFIX"/etc/swapspace.conf";
 
 static char *set_configfile(long long dummy)
 {
