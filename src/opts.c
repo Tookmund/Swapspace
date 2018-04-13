11df68cada1e9ca9292d6fcb0cd842326f32fbd2
Jacob Adams <tookmund@gmail.com>
2018-04-13 14:06:12 -0400

    opts: Trim copyright lines to 80 characters

--- src/opts.c
+++ src/opts.c
@@ -45,7 +45,8 @@ static const char copyright[] = "\n"
       "This program is free software, and is available for use under the GNU\n"
       "General Public License (GPL).\n"
       "See http://pqxx.org/development/swapspace\n"
-      "This is a fork of the original project by Jacob Adams hosted at https://github.com/Tookmund/swapspace\n";
+      "This is a fork of the original project by Jacob Adams hosted at:\n"
+	  "https://github.com/Tookmund/swapspace\n";
 
 
 #ifndef NO_CONFIG
