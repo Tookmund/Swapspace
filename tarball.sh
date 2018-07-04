372223ddca86bf177dbda168c5c9e5f1e5b7e695
Jacob Adams <tookmund@gmail.com>
2018-07-03 23:04:30 -0400

    tarball.sh: Remove
    Can be replaced with make dist

--- tarball.sh
+++
@@ -1,7 +0,0 @@
-VERSION=1.12
-autoreconf
-./configure
-make distclean
-tar cavf swapspace-$VERSION.tar.gz --exclude-vcs .
-echo "Signing swapspace-$VERSION.tar.gz"
-gpg --sign swapspace-$VERSION.tar.gz
